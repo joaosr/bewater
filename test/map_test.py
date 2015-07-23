bd12125fdb5a3ddd74b968ee8ed8a67ad3b98406
nairalirayoga <eng.jmsoares@gmail.com>
2015-07-22 23:14:39 -0300

    my django app

---
+++ test/map_test.py
@@ -0,0 +1,15 @@
+from .biomas.application.views  import biomas_map
+import unittest
+
+class TestMap(unittest.TestCase):
+    """docstring for TestMap"""
+    def __init__(self, arg):
+        super(TestMap, self).__init__()
+        self.arg = arg
+
+    def test_mapid(self):
+        _map = {"mapid": "ahsuahsuhsua", "token": "ashuahsuahusha"}
+        self.assertEqual(_map.keys(), ["mapid", "token"])
+
+    if __name__ == __main__:
+        unittest.main()
