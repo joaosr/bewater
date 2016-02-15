4979e54817de83e8961ac3c1e2c8c080ede613fc
joaosr <eng.jmsoares@gmail.com>
2016-02-15 16:44:36 -0300

    Created gulp build project

---
+++ bewater/core/static/gulpfile.js
@@ -0,0 +1,48 @@
+// /////////////////////////////////////////////////////////////
+// Required
+// /////////////////////////////////////////////////////////////
+
+var gulp = require('gulp'),
+    uglify = require('gulp-uglify')
+    compass = require('gulp-compass'),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename');
+
+// /////////////////////////////////////////////////////////////
+// Scripts Task
+// /////////////////////////////////////////////////////////////
+gulp.task('scripts', function(){
+    gulp.src(['app/js/**/*.js', '!app/js/**/*.min.js'])
+    .pipe(rename({suffix:'.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+});
+
+// /////////////////////////////////////////////////////////////
+// Compass / Sass Task
+// /////////////////////////////////////////////////////////////
+gulp.task('compass', function(){
+    gulp.src('app/scss/style.scss')
+    .pipe(plumber())
+    .pipe(compass({
+        config_file: './config.rb',
+        css: 'app/css',
+        sass: 'app/scss',
+        require: ['susy']
+    }))
+    .pipe(gulp.dest('app/css/'));
+});
+
+// /////////////////////////////////////////////////////////////
+// Watch Task
+// /////////////////////////////////////////////////////////////
+gulp.task('watch', function(){
+    gulp.watch('app/js/**/*.js', ['scripts']);
+    gulp.watch('app/scss/**/*.scss', ['compass']);
+});
+
+// /////////////////////////////////////////////////////////////
+// Default Task
+// /////////////////////////////////////////////////////////////
+gulp.task('default', ['scripts', 'watch']);
+
